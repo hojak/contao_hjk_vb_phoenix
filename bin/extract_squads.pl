64564adb578e893d57d0e89bd53d5283b77bad3c
Holger Janßen-Kroll <git@ho-jk.de>
2021-10-17 12:33:55 +0200

    doc: add usage hints

--- bin/extract_squads.pl
+++ bin/extract_squads.pl
@@ -1,5 +1,17 @@
 #! /usr/bin/perl
-# extrahiert aus den Links zum Ergebnisdienst Stallel-ID und Namen
+# extrahiert aus den Links zum Ergebnisdienst Staffel-ID und Namen
+#
+# Input: Gespeicherter HTML-Code einer Basisseite des WVV mit Links auf den Ergebnisdienst der einzelnen Staffeln
+#        z.B. https://www.volleyball.nrw/spielwesen/ergebnisdienst/frauen/ für die aktuellen Staffeln der Damen-Wettbewerbe
+#
+# Output: CSV-Datei, die als Eingabe zum Import der Staffeln im Contao-Backen verwendet werden kann.
+#
+# Hinweis: 
+# Bei den Jugend-Staffeln gibt es teilweise doppelte Einträge. Wenn alle Jugend-Staffeln importiert werden sollen,
+# empfielt es sich, zunächst alle Staffeln in eine Datei zusammen zu führen und anschließend Doppelte Einträge zu entfernen.
+# Eine solche Liste wird meist vor Beginn einer Saison unter /data diesem Repository hinzugefügt.
+
+$year = 2021;
 
 $input = join "", <>;
 
@@ -7,6 +19,6 @@ while ( $input =~ m#<a href="([^"]*)">([^<]*)</a>#g ) {
 	$name = $2;
 
 	if ( $1 =~ m#ergebnisdienst/detail/liga/([0-9]+)# ) {
-		print "$1;2017;$name;\n"; 	
+		print "$1;$year;$name;\n"; 	
 	}
 }
